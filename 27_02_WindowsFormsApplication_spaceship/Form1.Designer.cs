e8863978019f3dc4782a3fd032303930c97b5ff9
ondrej.sindelka <ondrej.sindelka@U1-23>
2025-02-27 10:17:45 +0100

    Add project files.

---
+++ 27_02_WindowsFormsApplication_spaceship/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace _27_02_WindowsFormsApplication_spaceship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picturBox_spaceship01 = new System.Windows.Forms.PictureBox();
+            this.button_up = new System.Windows.Forms.Button();
+            this.button_right = new System.Windows.Forms.Button();
+            this.button_left = new System.Windows.Forms.Button();
+            this.button_down = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picturBox_spaceship01)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picturBox_spaceship01
+            // 
+            this.picturBox_spaceship01.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picturBox_spaceship01.BackColor = System.Drawing.Color.Transparent;
+            this.picturBox_spaceship01.BackgroundImage = global::_27_02_WindowsFormsApplication_spaceship.Properties.Resources.upscalemedia_transformed;
+            this.picturBox_spaceship01.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picturBox_spaceship01.Location = new System.Drawing.Point(698, 96);
+            this.picturBox_spaceship01.Name = "picturBox_spaceship01";
+            this.picturBox_spaceship01.Size = new System.Drawing.Size(266, 157);
+            this.picturBox_spaceship01.TabIndex = 0;
+            this.picturBox_spaceship01.TabStop = false;
+            // 
+            // button_up
+            // 
+            this.button_up.Location = new System.Drawing.Point(600, 541);
+            this.button_up.Name = "button_up";
+            this.button_up.Size = new System.Drawing.Size(41, 40);
+            this.button_up.TabIndex = 1;
+            this.button_up.Text = "▲";
+            this.button_up.UseVisualStyleBackColor = true;
+            // 
+            // button_right
+            // 
+            this.button_right.Location = new System.Drawing.Point(641, 583);
+            this.button_right.Name = "button_right";
+            this.button_right.Size = new System.Drawing.Size(41, 40);
+            this.button_right.TabIndex = 2;
+            this.button_right.Text = "►";
+            this.button_right.UseVisualStyleBackColor = true;
+            this.button_right.Click += new System.EventHandler(this.button_right_Click);
+            // 
+            // button_left
+            // 
+            this.button_left.Location = new System.Drawing.Point(557, 583);
+            this.button_left.Name = "button_left";
+            this.button_left.Size = new System.Drawing.Size(41, 40);
+            this.button_left.TabIndex = 3;
+            this.button_left.Text = "◄";
+            this.button_left.UseVisualStyleBackColor = true;
+            this.button_left.Click += new System.EventHandler(this.button_left_Click);
+            // 
+            // button_down
+            // 
+            this.button_down.Location = new System.Drawing.Point(600, 626);
+            this.button_down.Name = "button_down";
+            this.button_down.Size = new System.Drawing.Size(41, 40);
+            this.button_down.TabIndex = 4;
+            this.button_down.Text = "▼";
+            this.button_down.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_27_02_WindowsFormsApplication_spaceship.Properties.Resources.R1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button_down);
+            this.Controls.Add(this.button_left);
+            this.Controls.Add(this.button_right);
+            this.Controls.Add(this.button_up);
+            this.Controls.Add(this.picturBox_spaceship01);
+            this.Name = "Form1";
+            this.Text = "Spaceship";
+            ((System.ComponentModel.ISupportInitialize)(this.picturBox_spaceship01)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picturBox_spaceship01;
+        private System.Windows.Forms.Button button_up;
+        private System.Windows.Forms.Button button_right;
+        private System.Windows.Forms.Button button_left;
+        private System.Windows.Forms.Button button_down;
+    }
+}
+
---
+++ 27_02_WindowsFormsApplication_spaceship/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _27_02_WindowsFormsApplication_spaceship
+{
+    public partial class Form1 : Form
+    {
+        Spaceship oddesey = new Spaceship(640,360);
+        
+        public Form1()
+        {
+            InitializeComponent();
+            picturBox_spaceship01.Location = new Point(oddesey.spaceX, oddesey.spaceY);
+        }
+
+        private void button_left_Click(object sender, EventArgs e)
+        {
+            int x = picturBox_spaceship01.Location.X;
+            int y = picturBox_spaceship01.Location.Y;
+            picturBox_spaceship01.Location = new Point(x-10, y);
+        }
+
+        private void button_right_Click(object sender, EventArgs e)
+        {
+            int x = picturBox_spaceship01.Location.X;
+            int y = picturBox_spaceship01.Location.Y;
+            picturBox_spaceship01.Location = new Point(x + 10, y);
+
+        }
+    }
+    public class Spaceship
+    {
+        public int spaceX;
+        public int spaceY;
+        public Spaceship(int midX, int mixY)
+        {
+            this.spaceX = midX;
+            this.spaceY = mixY;
+        }
+    }
+}
